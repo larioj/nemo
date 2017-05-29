5a022a0d271ea7e9dd0e9c737f86d9c3a3b7e44e
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-05-28 18:02:15 -0700

    Add example

--- src/Graph.hs
+++ src/Graph.hs
@@ -6,6 +6,8 @@ import Data.Set
     ( Set
     , toList
     , fromList
+    , member
+    , insert
     )
 
 import Control.Monad.Trans.State
@@ -31,14 +33,26 @@ class Graph g v e | g -> v e where
     add :: v -> g -> g
     map :: g -> (e -> e) -> v -> v
 
+dfv :: (Graph g v e) => g -> [v]
+dfv g = dfv' g (toList . vertices $ g) ([], fromList [])
+
+dfv' :: (Graph g v e) => g [v] ([v], Set v) -> ([v], Set v)
+dfv' g children st = foldl iter st chidren
+    where
+        --iter :: ([v], Set v) -> v -> ([v], Set v)
+        iter (prog, seen) v = if (v `member` seen) then (prog, seen)
+            else
+                let (prog', seen') = dfv' g (toList . children g $ v) (prog, insert v seen)
+                in (v : prog', seen')
+
 dfFold :: (Graph g v e, Ord v) => (a -> v -> a) -> a -> g -> a
 dfFold f init g = runStateCtl (dfv' . vertices $ g) $ init
-    where dfv' = mapM_ iter . toList
+    where dfv' vs = mapM_ iter (toList vs)
           iter v = seen v >>=
                    choose (return ()) (recur v)
           recur v = record v >>= \_ ->
-                     (dfv' . children g $ v) >>= \_ ->
-                     transform f v
+                    (dfv' . children g $ v) >>= \_ ->
+                    transform f v
 
 topoSort :: (Graph g v e, Ord v) => g -> [v]
 topoSort = dfFold (flip (:)) []
