4a6235a0460904fe4a93f583e16b3364d2c4ee68
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-05-29 18:29:25 -0700

    Fix typo

--- src/Nemo.hs
+++ src/Nemo.hs
@@ -19,25 +19,25 @@ import Graph
 type Nemo k = (Graph k, Graph k, Graph k)
 
 update :: Ord k => (Nemo k -> k -> k) -> Nemo k -> Nemo k
-update f (dependencyGraph, predesossorGraph, cloneGraph) =
+update f (dependencyGraph, predecessorGraph, cloneGraph) =
     dfv seeds ctl accfn init dependencyGraph
     where
         seeds = Map.keysSet dependencyGraph
         ctl = Map.keysSet cloneGraph
         accfn = update' f
-        init = (dependencyGraph, predesossorGraph,cloneGraph)
+        init = (dependencyGraph, predecessorGraph,cloneGraph)
 
 update' :: Ord k => (Nemo k -> k -> k) -> Nemo k -> k -> Nemo k
-update' f (dependencyGraph, predesossorGraph, cloneGraph) original =
+update' f (dependencyGraph, predecessorGraph, cloneGraph) original =
     (newDependencyGraph, newPredecessorGraph, newCloneGraph)
     where
         originalDependencies = findWithDefault Set.empty original dependencyGraph
-        clone = f (dependencyGraph, predesossorGraph, cloneGraph) original
+        clone = f (dependencyGraph, predecessorGraph, cloneGraph) original
         cloneDependencies = Set.map (getDependencyClone cloneGraph) originalDependencies
         newDependencyGraph = Map.insert clone cloneDependencies dependencyGraph
         newCloneGraph = Map.insert original (Set.singleton clone) cloneGraph
         oldClone = findWithDefault Set.empty original cloneGraph
-        newPredecessorGraph = Map.insert clone oldClone predesossorGraph
+        newPredecessorGraph = Map.insert clone oldClone predecessorGraph
 
 getDependencyClone :: Ord k => Graph k -> k -> k
 getDependencyClone cloneGraph dep =
