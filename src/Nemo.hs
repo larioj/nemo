05070418213db4e890c93672408319c906b7b627
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-05-29 18:26:36 -0700

    Add update

---
+++ src/Nemo.hs
@@ -0,0 +1,44 @@
+module Nemo
+    ( Nemo
+    , update
+    ) where
+
+import qualified Data.Set as Set
+import Data.Set
+    ( Set
+    )
+import qualified Data.Map as Map
+import Data.Map
+    ( findWithDefault
+    )
+import Graph
+    ( Graph
+    , dfv
+    )
+
+type Nemo k = (Graph k, Graph k, Graph k)
+
+update :: Ord k => (Nemo k -> k -> k) -> Nemo k -> Nemo k
+update f (dependencyGraph, predesossorGraph, cloneGraph) =
+    dfv seeds ctl accfn init dependencyGraph
+    where
+        seeds = Map.keysSet dependencyGraph
+        ctl = Map.keysSet cloneGraph
+        accfn = update' f
+        init = (dependencyGraph, predesossorGraph,cloneGraph)
+
+update' :: Ord k => (Nemo k -> k -> k) -> Nemo k -> k -> Nemo k
+update' f (dependencyGraph, predesossorGraph, cloneGraph) original =
+    (newDependencyGraph, newPredecessorGraph, newCloneGraph)
+    where
+        originalDependencies = findWithDefault Set.empty original dependencyGraph
+        clone = f (dependencyGraph, predesossorGraph, cloneGraph) original
+        cloneDependencies = Set.map (getDependencyClone cloneGraph) originalDependencies
+        newDependencyGraph = Map.insert clone cloneDependencies dependencyGraph
+        newCloneGraph = Map.insert original (Set.singleton clone) cloneGraph
+        oldClone = findWithDefault Set.empty original cloneGraph
+        newPredecessorGraph = Map.insert clone oldClone predesossorGraph
+
+getDependencyClone :: Ord k => Graph k -> k -> k
+getDependencyClone cloneGraph dep =
+    head . Set.elems . findWithDefault (Set.singleton dep) dep $ cloneGraph
