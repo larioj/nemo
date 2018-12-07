247e786f1cab6792c85e6ba7095e1b6aa375f680
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2018-12-07 00:39:32 -0800

    Add init

--- src/Data/Nemo/Env.hs
+++ src/Data/Nemo/Env.hs
@@ -2,12 +2,14 @@
 
 module Data.Nemo.Env where
 
-import           Control.Lens           (makeLenses)
+import           Control.Lens           (makeLenses, (^.))
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.Nemo.Error        (maybeDie)
 import qualified Data.Nemo.Error        as Err
+import qualified Nemo.Config            as Config
 import           Nemo.Util              (findMarker)
 import           System.FilePath        (takeDirectory, (</>))
+import           System.Nemo            (touchDirectory)
 
 data Env = Env
   { _base     :: FilePath
@@ -18,13 +20,23 @@ data Env = Env
 
 makeLenses ''Env
 
-env :: MonadIO m => String -> m Env
-env marker = do
-  nemo <- maybeDie Err.UnableToFindMarker =<< liftIO (findMarker marker)
-  return $
-    Env
-      { _base = takeDirectory nemo
-      , _nemo = nemo
-      , _sources = nemo </> "src"
-      , _metadata = nemo </> "metadata"
-      }
+loadEnv :: MonadIO m => String -> m Env
+loadEnv marker = do
+  fmap env . maybeDie Err.UnableToFindMarker =<< liftIO (findMarker marker)
+
+env :: String -> Env
+env nemo =
+  Env
+    { _base = takeDirectory nemo
+    , _nemo = nemo
+    , _sources = nemo </> Config.srcName
+    , _metadata = nemo </> Config.metadataName
+    }
+
+init :: MonadIO m => m ()
+init = do
+  liftIO $ touchDirectory (e ^. nemo)
+  liftIO $ touchDirectory (e ^. sources)
+  liftIO $ touchDirectory (e ^. metadata)
+  where
+    e = env Config.libName
---
+++ src/Nemo/Config.hs
@@ -0,0 +1,10 @@
+module Nemo.Config where
+
+libName :: String
+libName = "nemolib"
+
+srcName :: String
+srcName = "src"
+
+metadataName :: String
+metadataName = "metadata"
